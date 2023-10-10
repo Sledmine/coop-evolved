2696a1470391aed000d602257c51f069492862b1
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-10-10 01:01:53 -0600

    improve player coop ready event

--- lua/coop/network/events.lua
+++ lua/coop/network/events.lua
@@ -19,10 +19,37 @@ blam.rcon.event("ChangeBiped", function(message, playerIndex)
 end)
 
 blam.rcon.event("UpdateVotes", function(message, playerIndex)
-    if blam.isGameHost() then
+    if not blam.isGameSAPP() then
         local votes = tonumber(message)
         if votes then
             CoopState.remainingVotes = votes
+        end
+    end
+end)
+
+blam.rcon.event("Ready", function(message, playerIndex)
+    if blam.isGameSAPP() then
+        if CoopServerState.playersReady[playerIndex] then
+            return
+        end
+        CoopServerState.remainingVotes = CoopServerState.remainingVotes - 1
+        CoopServerState.playersReady[playerIndex] = true
+
+        if CoopServerState.remainingVotes <= 0 then
+            console_out("All players are ready, starting game...")
+
+            CoopStarted = true
+            local currentMapName = get_var(0, "$map")
+            local splitName = currentMapName:split "_"
+            local baseNoCoopName = splitName[1]
+            coop.enableSpawn(true)
+            execute_script("wake main_" .. baseNoCoopName)
+
+        else
+            console_out(
+                "Player " .. playerIndex .. " is ready, " .. CoopServerState.remainingVotes ..
+                    " votes remaining")
+            blam.rcon.dispatch("UpdateVotes", tostring(CoopServerState.remainingVotes))
         end
     end
 end)
--- lua/coop_evolved_server.lua
+++ lua/coop_evolved_server.lua
@@ -1,21 +1,22 @@
 api_version = "1.12.0.0"
+DebugMode = true
 
 local blam = require "blam"
+require "luna"
 
 local coop = require "coop.coop"
 local constants = require "coop.constants"
 local events = require "coop.network.events"
 
 local customPlayerBipeds = {}
-local CoopStarted = false
+CoopServerState = {remainingVotes = 0, difficulty = coop.difficulties[1], playersReady = {}}
 
 function GetReadyForCoop(playerIndex)
     if not CoopStarted then
         local currentMapName = get_var(0, "$map")
         local playersCount = tonumber(get_var(0, "$pn"))
-        local requiredVotes = coop.getRequiredVotes(playersCount)
-        --Broadcast("@i," .. coop.getRequiredVotes(playersCount) .. ",4")
-        
+        -- local requiredVotes = coop.getRequiredVotes(playersCount)
+
         rprint(playerIndex, "sync_camera_control 1")
         -- a50
         rprint(playerIndex, "sync_camera_set insertion_3 0")
@@ -31,15 +32,21 @@ function GetReadyForCoop(playerIndex)
         -- Dispatch coop menu event
         blam.rcon.dispatch("CoopMenu")
 
-        blam.rcon.dispatch("UpdateVotes", tostring(requiredVotes), playerIndex)
+        blam.rcon.dispatch("UpdateVotes", tostring(CoopServerState.remainingVotes))
     end
 end
 
 function OnPlayerJoin(playerIndex)
+    CoopServerState.remainingVotes = CoopServerState.remainingVotes + 1
     GetReadyForCoop(playerIndex)
 end
 
 function OnPlayerLeave(playerIndex)
+    if CoopServerState.playersReady[playerIndex] then
+        CoopServerState.remainingVotes = CoopServerState.remainingVotes + 1
+    end
+    CoopServerState.remainingVotes = CoopServerState.remainingVotes - 1
+    CoopServerState.playersReady[playerIndex] = nil
     coop.findNewSpawn(playerIndex)
 end
 
