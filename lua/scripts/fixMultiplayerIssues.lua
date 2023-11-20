5a9f9e7e7250462abd37a0954febc6b8383c8a24
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-11-20 13:24:41 -0600

    improve AI team assignment

--- lua/scripts/fixMultiplayerIssues.lua
+++ lua/scripts/fixMultiplayerIssues.lua
@@ -1,72 +1,92 @@
 local tag = require "lua.modules.tag"
 
-local scenarioTagPath = arg[1]:replace("\\", "/"):replace("tags/", "")
-local defaultAITeam = arg[2]
+local scenarioTagPath = arg[1]
+assert(scenarioTagPath, "No scenario tag path provided")
+scenarioTagPath = scenarioTagPath:replace("\\", "/"):replace("tags/", "")
 
 print("Converting scenario type to multiplayer...")
+
+local maps = {
+    "a10", -- The Pillar of Autumn
+    "a30", -- Halo
+    "a50", -- The Truth and Reconciliation
+    "b30", -- The Silent Cartographer
+    "b40", -- Assault on the Control Room
+    "c10", -- 343 Guilty Spark
+    "c20", -- The Library
+    "c40", -- Two Betrayals
+    "d20", -- Keyes
+    "d40" -- The Maw
+}
+
 tag.edit(scenarioTagPath, {type = "multiplayer"})
 
-local convenantEncounterLike = {
-    "cov",
-    "ghost",
-    "banshee",
-    "wraith",
-    "hunters",
-    "enc_swamp",
-    "bottom",
-    "top",
-    "flee",
-    -- B40
-    "a_bridge",
-    "b3_bridge",
-    "b4_bridge",
-    "c_bridge",
-    "b4_hall"
+local scenarioTeams = {
+    "default_by_unit",
+    "player",
+    "human",
+    "covenant",
+    "flood",
+    "sentinel",
+    "unused6",
+    "unused7",
+    "unused8",
+    "unused9"
+}
+local multiplayerTeams = {"red", "blue"}
+local allegianceTeams = {"default_by_unit", "player", "human", "sentinel"}
+local teamColor = {
+    default_by_unit = "\27[31m", -- Red
+    player = "\27[42m", -- Green
+    human = "\27[34m", -- Blue
+    covenant = "\27[35m", -- Magenta
+    flood = "\27[33m", -- Yellow
+    sentinel = "\27[36m", -- Cyan
+    unknown = "\27[41m" -- Red
 }
 
-local marineEncounterLike = {
-    "marine",
-    "tank",
-    "jeep",
-    "hangar_captain",
-    "prison",
-    -- B40
-    "test_hum"
-}
+print("Converting scenario teams...")
 
-local sentinelEncounterLike = {"sents", "sentinels", "monitor"}
-
-local floodEncounterLike = {"flood", "infection", "inf", "inc_swamp"}
-
-local findName = function(name)
-    return function(v, k)
-        return name:includes(v)
-    end
+local actorPalette = {}
+local actorPaletteCount = tag.count(scenarioTagPath, "actor_palette")
+for actorPaletteIndex = 0, actorPaletteCount - 1 do
+    local actorPaletteTag = tag.get(scenarioTagPath,
+                                    "actor_palette[" .. actorPaletteIndex .. "].reference")
+    assert(type(actorPaletteTag) == "string", "Failed to get actor palette tag")
+    local actorUnit = tag.get(actorPaletteTag, "unit")
+    assert(type(actorUnit) == "string", "Failed to get actor palette unit")
+    local actorTeam = tag.get(actorUnit, "default_team")
+    actorPalette[actorPaletteIndex] = actorTeam
 end
 
 local encounters = {}
 local encounterCount = tag.count(scenarioTagPath, "encounters")
 for encounterIndex = 0, encounterCount - 1 do
+    local encounterTeam = "default_by_unit"
     local encounterName = tag.get(scenarioTagPath, "encounters[" .. encounterIndex .. "].name")
-    assert(encounterName, "Encounter name is nil!")
-    encounterName = tostring(encounterName)
+    print("Encounter:", encounterName)
 
-    encounters[encounterIndex + 1] = {}
-    local encounter = encounters[encounterIndex + 1]
-    if table.find(convenantEncounterLike, findName(encounterName)) then
-        encounter.team_index = "covenant"
-    elseif table.find(marineEncounterLike, findName(encounterName)) then
-        -- Default by unit means index 0 or "red team" in multiplayer games
-        -- That equals to player team for coop purposes
-        encounter.team_index = "default_by_unit"
-    elseif table.find(sentinelEncounterLike, findName(encounterName)) then
-        encounter.team_index = "sentinel"
-        -- encounter.team_index = "default_by_unit"
-    elseif table.find(floodEncounterLike, findName(encounterName)) then
-        encounter.team_index = "flood"
-    elseif defaultAITeam then
-        encounter.team_index = defaultAITeam
+    local squadCount = tag.count(scenarioTagPath, "encounters[" .. encounterIndex .. "].squads")
+    for squadIndex = 0, squadCount - 1 do
+        local actorTypeIndex = tag.get(scenarioTagPath, "encounters[" .. encounterIndex ..
+                                           "].squads[" .. squadIndex .. "].actor_type")
+        if actorTypeIndex then
+            encounterTeam = actorPalette[actorTypeIndex]
+            break
+        end
     end
-    print(encounterName, "->", encounter.team_index or "UNKNOWN")
+    local color = teamColor[encounterTeam] or teamColor.unknown
+    print("Team:\t\t" .. color .. encounterTeam .. "\27[0m")
+    if encounterTeam == "default_by_unit" then
+        print("No team is assigned, it may not work as expected, check if this is correct!")
+    end
+
+    -- Convert to multiplayer team
+    if table.flip(allegianceTeams)[encounterTeam] then
+        encounterTeam = "default_by_unit"
+    end
+
+    encounters[encounterIndex + 1] = {team_index = encounterTeam}
+
 end
 tag.edit(scenarioTagPath, {encounters = encounters})
