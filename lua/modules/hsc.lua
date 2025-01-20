7cd51282b8838ff1c22db92ad3d885c80102e50b
Sledmine <40512223+Sledmine@users.noreply.github.com>
2025-01-20 02:09:51 -0600

    update bsc bridge module, fix args escaping

--- lua/modules/hsc.lua
+++ lua/modules/hsc.lua
@@ -1,10 +1,12 @@
 local hsc = {}
 
-local executeScript = Engine.hsc.executeScript
-
 local luna = require "luna"
-
 local hscDoc = require "hscDoc"
+local balltze = Balltze
+local engine = Engine
+local executeScript = engine.hsc.executeScript
+
+math.randomseed(os.time())
 
 local cacheHscGlobals = {
     boolean = "lua_boolean",
@@ -18,14 +20,15 @@ local cacheHscGlobals = {
 }
 
 local function getScriptArgs(args)
-    -- return table.map(args, function(arg)
-    --    if type(arg) == "string" then
-    --        return "\"" .. arg .. "\""
-    --    else
-    --        return tostring(arg)
-    --    end
-    -- end)
-    return table.map(args, tostring)
+    return table.map(args, function(v, k)
+        if type(v) == "string" then
+            local isSubExpression = v:startswith("(") and v:endswith(")")
+            if not isSubExpression and (v:includes(" ") or v == "") then
+                return "\"" .. v .. "\""
+            end
+        end
+        return tostring(v)
+    end)
 end
 
 local function getFunctionInvokation(hscFunction, args)
@@ -41,6 +44,43 @@ local function getVariable(varName)
     return get_global(varName)
 end
 
+-- Reimplement HSC functions with Lua
+
+function hsc.begin_random(functions)
+    local functions = table.copy(functions)
+    local function random()
+        local index = math.random(1, #functions)
+        local func = functions[index]
+        table.remove(functions, index)
+        return func
+    end
+    while #functions > 0 do
+        random()()
+    end
+end
+
+-- TODO Adapt depending on server or client version
+local hscGlobalsPointer = 0x0064bab0
+
+local difficulties = {"easy", "normal", "hard", "impossible"}
+function hsc.game_difficulty_get()
+    local hscGlobals = read_dword(hscGlobalsPointer)
+    local difficulty = read_byte(hscGlobals + 0xe)
+    return difficulties[difficulty + 1]
+end
+hsc.game_difficulty_get_real = hsc.game_difficulty_get
+
+function hsc.print(message)
+    engine.core.consolePrint("{}", tostring(message))
+end
+
+function hsc.cinematic_skip_start_internal()
+end
+
+function hsc.cinematic_skip_stop_internal()
+end
+
+-- Bind existing in game HSC functions to Lua
 setmetatable(hsc, {
     __index = function(_, key)
         local hscFunction = table.find(hscDoc, function(doc)
@@ -48,6 +88,7 @@ setmetatable(hsc, {
         end)
         if hscFunction then
             local returnType = hscFunction.returnType
+            -- Handle compatible primitive types trough Lua
             if returnType == "boolean" or returnType == "short" or returnType == "long" or
                 returnType == "real" then
                 return function(...)
@@ -56,34 +97,22 @@ setmetatable(hsc, {
                     setVariable(cacheHscGlobals[returnType], functionInvokation)
                     local result = getVariable(cacheHscGlobals[returnType])
                     if returnType == "boolean" then
-                        if key == "objects_can_see_object" then
-                            logger:debug("{} -> {}", functionInvokation, result)
-                        end
                         result = luna.bool(result)
                     end
                     return result
                 end
-                -- elseif returnType == "object" or returnType == "unit" or returnType == "object_list" then
-                --    return function(...)
-                --        local args = table.map({...}, tostring)
-                --        local functionInvokation = getFunctionInvokation(hscFunction, args)
-                --        executeScript(functionInvokation)
-                --        return "(" .. cacheHscGlobals[returnType] .. ")"
-                --    end
-            elseif returnType == "object" or returnType == "unit" or returnType == "object_list" then
+                -- Let game hs script execution handle not primitive types
+            elseif returnType ~= "void" then
                 return function(...)
-                    local args = table.map({...}, tostring)
+                    local args = getScriptArgs({...})
                     local functionInvokation = getFunctionInvokation(hscFunction, args)
                     return "(" .. functionInvokation .. ")"
                 end
-            elseif returnType ~= "void" then
-                logger:warning("Return type {} for {} not implemented", returnType, key)
             else
                 return function(...)
-                    local args = table.map({...}, tostring)
-                    logger:debug("Invoking function {}", key)
+                    local args = getScriptArgs({...})
                     local functionInvokation = getFunctionInvokation(hscFunction, args)
-                    logger:debug(functionInvokation)
+                    logger:debug("Executing: {}", functionInvokation)
                     executeScript(functionInvokation)
                 end
             end
