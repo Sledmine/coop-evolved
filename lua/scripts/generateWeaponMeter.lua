f5bb3824c7fc55cc0f4453f4822ff9bc22b32e17
Sledmine <40512223+Sledmine@users.noreply.github.com>
2026-02-26 01:48:34 -0600

    Add prototype meter weapon generator

---
+++ lua/scripts/generateWeaponMeter.lua
@@ -0,0 +1,92 @@
+local argparse = require "lua.scripts.modules.argparse"
+
+local parser = argparse("generateWeaponMeter",
+                        "Generates a weapon meter image from given parameters.")
+parser:argument("imagePath", "Path to the output image file.")
+parser:argument("ammoCount", "Number of ammo units to display."):convert(tonumber)
+parser:argument("direction", "Direction of the meter (horizontal or vertical)."):choices{
+    "horizontal",
+    "-horizontal",
+    "vertical",
+    "-vertical"
+}
+parser:option("levels", "Number of levels in the meter."):default(1):convert(tonumber)
+
+local args = parser:parse()
+
+local imagePath = args.imagePath
+local ammoCount = args.ammoCount
+local direction = args.direction
+local levels = args.levels
+local meterPath = "meter.png"
+
+-- Use image magick to get data from the image and generate meter
+local function magick(...)
+    local args = {...}
+    local command = "magick " .. table.concat(args, " ")
+    local handle = io.popen(command)
+    local result = handle:read("*a")
+    -- print("Executed command:", command)
+    -- print("Result:", result)
+    handle:close()
+    return result
+end
+
+-- Get image dimensions
+local dimensions = magick("identify", "-format", "\"%w %h\"", args.imagePath)
+local width, height = dimensions:match("(%d+) (%d+)")
+width, height = tonumber(width), tonumber(height)
+print("Image dimensions:", width, height)
+
+local meterWidth, meterHeight
+if args.direction:find("horizontal") then
+    meterWidth = width * args.ammoCount
+    meterHeight = height
+else
+    meterWidth = width
+    meterHeight = height * args.ammoCount
+end
+
+-- Create a new image for the meter, make sure color is RGB
+magick("convert", "-size", meterWidth .. "x" .. meterHeight, "xc:none", meterPath)
+
+-- Composite the ammo units onto the meter image
+for i = 0, ammoCount - 1 do
+    local offsetX = 0
+    local offsetY = 0
+    if args.direction:find("horizontal") then
+        offsetX = i * width
+    else
+        offsetY = i * height
+    end
+    magick("composite", "-geometry", "+0+0", args.imagePath, "-geometry",
+           "+" .. offsetX .. "+" .. offsetY, meterPath, meterPath)
+end
+
+-- Final diffuse meter image
+print("Weapon meter generated at:", meterPath)
+
+
+-- Now lets generate the alpha mask for the meter
+local maskPath = "meter_mask.png"
+magick("convert", "-size", meterWidth .. "x" .. meterHeight, "xc:black", maskPath)
+
+for i = 0, ammoCount - 1 do
+    local offsetX = 0
+    local offsetY = 0
+    if args.direction:find("horizontal") then
+        offsetX = i * width
+    else
+        offsetY = i * height
+    end
+
+    local value = math.min(i + 1, 255)
+    local color = "'rgb(" .. value .. "," .. value .. "," .. value .. ")'"
+    local rect = "'rectangle " .. offsetX .. "," .. offsetY .. " " .. (offsetX + width - 1) .. "," .. (offsetY + height - 1) .. "'"
+    magick("convert", maskPath, "-fill", color, "-draw", rect, maskPath)
+end
+
+-- Mix into one final image with alpha channel, make sure it is set as RGBA
+local finalPath = "weapon_meter.png"
+magick("convert", meterPath, maskPath, "-alpha", "off", "-compose", "copy_opacity", "-composite", finalPath)
+print("Weapon meter with alpha mask generated at:", finalPath)
